ba566ef4c16a782024246f68da1b54ab6a0f0278
aditya sahu <adityasahu@adityas-MacBook-Air.local>
2026-01-06 23:09:25 +0530

    Add assets to cgi-issaquah

---
+++ cgi-issaquah/js/index.js
@@ -0,0 +1,57 @@
+// Button Hover Animate
+const buttons = document.querySelectorAll(".btn");
+
+buttons.forEach((btn) => {
+  const bg = btn.querySelector(".btn-bg");
+
+  btn.addEventListener("mouseenter", (e) => {
+    const rect = btn.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    bg.style.left = `${x}px`;
+    bg.style.top = `${y}px`;
+    bg.style.transform = "translate(-50%, -50%) scale(28)";
+  });
+
+  btn.addEventListener("mouseleave", (e) => {
+    const rect = btn.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    bg.style.left = `${x}px`;
+    bg.style.top = `${y}px`;
+    bg.style.transform = "translate(-50%, -50%) scale(0)";
+  });
+});
+
+// Slider Setting
+$('.photos-slider').slick({
+  centerMode: true,
+  centerPadding: '0px',
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  touchThreshold: 50,
+  edgeFriction: 1,
+  autoplay: true,
+  autoplaySpeed: 1000,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        slidesToShow: 1
+      }
+    }
+  ]
+});
