7294938bade1a519cbe0e00955076ecc287d6ed7
aditya sahu <adityasahu@adityas-MacBook-Air.local>
2026-01-07 22:48:15 +0530

    Mobile CSS/JS Updates

--- cgi-issaquah/js/index.js
+++ cgi-issaquah/js/index.js
@@ -25,36 +25,46 @@ buttons.forEach((btn) => {
   });
 });
 
-// Slider Setting
+
+
 $('.photos-slider').slick({
-  centerMode: true,
-  centerPadding: '0px',
-  slidesToShow: 5,
-  slidesToScroll: 1,
-  touchThreshold: 50,
-  edgeFriction: 1,
-  autoplay: true,
-  autoplaySpeed: 1000,
+  dots: false,
+  infinite: true,
   arrows: false,
+  touchThreshold: 50,
+  speed: 500,
+  slidesToShow: 5,
+  slidesToScroll: 5,
+  autoplay: true,
+  autoplaySpeed: 5000,
   responsive: [
     {
-      breakpoint: 768,
+      breakpoint: 1024,
       settings: {
-        arrows: false,
-        centerMode: true,
-        slidesToShow: 3
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
       }
     },
     {
       breakpoint: 480,
       settings: {
-        arrows: false,
-        centerMode: true,
-        slidesToShow: 1
+        slidesToShow: 1,
+        slidesToScroll: 1
       }
     }
   ]
 });
+				
+
 
 // menu Toggle JS
 document.addEventListener("DOMContentLoaded", () => {
