f93e5d5034557a9d5afc0c2900b27cf2cd99657e
aditya sahu <adityasahu@adityas-MacBook-Air.local>
2026-01-08 02:25:23 +0530

    Slider Fixes  Updates

--- cgi-issaquah/js/index.js
+++ cgi-issaquah/js/index.js
@@ -26,25 +26,26 @@ buttons.forEach((btn) => {
 });
 
 
-
 $('.photos-slider').slick({
   dots: false,
   infinite: true,
   arrows: false,
-  touchThreshold: 50,
+  swipe: true,
+  touchThreshold: 10,
   speed: 500,
   slidesToShow: 5,
   slidesToScroll: 5,
   autoplay: true,
   autoplaySpeed: 5000,
+  pauseOnFocus: false,
+  adaptiveHeight: false,     // VERY important
+
   responsive: [
     {
       breakpoint: 1024,
       settings: {
         slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: false
+        slidesToScroll: 3
       }
     },
     {
@@ -63,6 +64,7 @@ $('.photos-slider').slick({
     }
   ]
 });
+
 				
 
 
