6fe12f048e712439cfa239c96965fed824984b9d
aditya sahu <adityasahu@adityas-MacBook-Air.local>
2026-01-23 03:55:06 +0530

    Form CSS & JS Updates

--- kitchen-kindness/js/index.js
+++ kitchen-kindness/js/index.js
@@ -107,20 +107,3 @@ $(".gallery-slider").slick({
     },
   ],
 });
-
-
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll(".ckk-wrapper .form-radio-item label").forEach(label => {
-    const text = label.textContent.trim();
-    if (!text.startsWith("$")) return;
-
-    const parts = text.split(" ");
-    const amount = parts.shift();
-    const rest = parts.join(" ");
-
-    label.innerHTML = `
-      <span class="donation-amount">${amount}</span>
-      <span class="donation-text">${rest}</span>
-    `;
-  });
-});
